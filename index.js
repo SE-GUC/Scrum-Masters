3a4740b592a8025b1cf3fcbf7656d3a19f9d64d5
mostafa waleed <mostafawaleed5470@gmail.com>
2019-03-01 15:50:09 +0200

    formating the code

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
 const comment = require('./routes/api/comment')
 
-app.use(express.json());
+app.use(express.json())
 app.get('/', (req, res) => {
   res.send(`<h1>Welcome to Our website</h1>
   <a href="/api/comment">comments</a>`
-  );
+  )
 })
 
 app.use('/api/comment', comment)
 
-app.get("/", (req, res) => {
-  res.send(`<h1>Welcome to our Scrum Master's Website</h1>`);
-});
+app.get('/', (req, res) => {
+  res.send(`<h1>Welcome to our Scrum Master's Website</h1>`)
+})
 
 // Handling 404
 app.use((req, res) => {
-  res.status(404).send({ err: "We can not find what you are looking for" });
-});
+  res.status(404).send({ err: 'We can not find what you are looking for' })
+})
 
-const port =4001;
+const port = 4001
 app.listen(port, () =>
   console.log(`Server is up and running on server ${port}`)
-);
+)
