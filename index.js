6fb87f29baed5a5a45a4d62bad2b0efab9df724e
mostafa waleed <mostafawaleed5470@gmail.com>
2019-04-05 19:25:35 +0200

    -

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.use((req, res) => {
   res.status(404).send({ err: 'We can not find what you are looking for' })
 })
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 
 app.listen(port, () =>
   console.log(`Server is up and running on server ${port}`)
