72a396c917fc7a154edb1fc3db633a35e0e59a3a
decapood <omar.abdelkarim@student.guc.edu.eg>
2019-04-07 16:28:41 +0200

    organizing the components

--- scrum-masters/src/App.js
+++ scrum-masters/src/App.js
@@ -8,95 +8,123 @@ axios.defaults.adapter = require('axios/lib/adapters/http')
 
 
 class App extends Component {
-  state ={
-    count :0 ,
-    company:[]
-  }
+  //(Here we're suppossed to just call the classes and decide the appropriate devisions and navigations)
+//   state ={
+//     count :0 ,
+//     company:[]
+//   }
   
  
-  showCompnies=()=> {
+//   showCompnies=()=> {
     
-    axios.get('http://localhost:3001/api/company')
-   .then(companies=>{
-     this.setState({company:companies.data})
-     this.setState({count:this.state.count+1})
-   })
-   .catch(err=>{
-     console.log(err)
-     
-   })
-  
-
-  }
-  
-  rendercompanies=()=>{
-    if(this.state.count===0) return null 
-    else if(this.state.company.length ===0) return <Badge style ={{fontSize:15}}variant="primary">No Companies</Badge>
-    else{
-     return <ul>{this.state.company.map(companies=><li key={companies._id}> <ListGroup.Item action href="#link1" action variant="light">{companies.company_name_english}</ListGroup.Item></li>)}</ul>
-    }
-  }
-  
-  render () {
-    return (
-     <div>
-       <Navbar  bg="light" color ="skyblue" expand="lg">
-  <Navbar.Brand href="#home">Sumerge</Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#link">Notifications</Nav.Link>
-      <Nav.Link href="#link">Assigned Companies</Nav.Link>
-
-      <NavDropdown title="" id="basic-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Sign out</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">settings</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Contact us</NavDropdown.Item>
-      </NavDropdown>
-    </Nav>
-  </Navbar.Collapse>
-</Navbar>
-       <span style = {{fontSize:30,fontWeight:"italic",color:"skyblue"}} className ="badge">Companies</span> 
-       <br></br>
-       <Button onClick = {this.showCompnies} className=" m-2" variant="outline-secondary">Show Companies</Button>
-       {this.rendercompanies()}
-       
-       
-     
-       </div>
-       
-       )
-       
-    
-  }
-  
-//  getTheCompanies(){
- // <ul>{this.state.company.map(companies=><li key={companies._id}>{companies.company_name_english}</li>)}</ul>
-//    axios.get('http://localhost:3001/api/company')
+//     axios.get('http://localhost:3001/api/company')
 //    .then(companies=>{
-//      console.log(companies.data)
-//      return <ul>{companies.data.map(company=><li key={company._id}>{company.company_name_english}</li>)}</ul>
+//      this.setState({company:companies.data})
+//      this.setState({count:this.state.count+1})
 //    })
 //    .catch(err=>{
 //      console.log(err)
      
 //    })
-//  } 
-// company(){
-//   try{
-//   return async companies=>{
-//     companies = await axios.get('http://localhost:3001/api/company')
-//     const res = companies.data
-//     companies(res)
-//   }
-// }catch(e){
-//   console.log(e)
-// }
-// }
+  
 
+//   }
+  
+//   rendercompanies=()=>{
+//     if(this.state.count===0) return null 
+//     else if(this.state.company.length ===0) return <Badge style ={{fontSize:15}}variant="primary">No Companies</Badge>
+//     else{
+//      return <ul>{this.state.company.map(companies=><li key={companies._id}> <ListGroup.Item action href="#link1" action variant="light">{companies.company_name_english}</ListGroup.Item></li>)}</ul>
+//     }
+//   }
+  
+//   render () {
+//     return (
+//      <div>
+//        <Navbar  bg="light" color ="skyblue" expand="lg">
+//   <Navbar.Brand href="#home">Sumerge</Navbar.Brand>
+//   <Navbar.Toggle aria-controls="basic-navbar-nav" />
+//   <Navbar.Collapse id="basic-navbar-nav">
+//     <Nav className="mr-auto">
+//       <Nav.Link href="#home">Home</Nav.Link>
+//       <Nav.Link href="#link">Notifications</Nav.Link>
+//       <Nav.Link href="#link">Assigned Companies</Nav.Link>
+
+//       <NavDropdown title="" id="basic-nav-dropdown">
+//         <NavDropdown.Item href="#action/3.1">Sign out</NavDropdown.Item>
+//         <NavDropdown.Item href="#action/3.2">settings</NavDropdown.Item>
+//         <NavDropdown.Divider />
+//         <NavDropdown.Item href="#action/3.4">Contact us</NavDropdown.Item>
+//       </NavDropdown>
+//     </Nav>
+//   </Navbar.Collapse>
+// </Navbar>
+//        
+       
+       
+     
+//        </div>
+       
+//        )
+       
+    
+//   }
  
 }
+//showing all companies
+class Companies extends Component{
+  state = {
+      count :0 ,
+      company:[]
+    }
+  showCompanies=()=> {
+    
+      axios.get('http://localhost:3001/api/company')
+     .then(companies=>{
+       this.setState({company:companies.data})
+       this.setState({count:this.state.count+1})
+     })
+     .catch(err=>{
+       console.log(err)
+       
+     })
+    
+  
+    } 
+    render(){
+      return (
+        <dev>
+          <span style = {{fontSize:30,fontWeight:"italic",color:"skyblue"}} className ="badge">Companies</span> 
+//        <br></br>
+//        <Button onClick = {this.showCompnies} className=" m-2" variant="outline-secondary">Show Companies</Button>
+//        {this.rendercompanies()}
+        </dev>
+      )
+    } 
+}
+class CompanyView extends Component{
+state = {
+  company : {}
+}
+getCompany=() => {
+  axios.get('http://localhost:3001/api/company/:id')
+  .then(company=>{
+    this.setState({company:this.state.company})
+  })
+  .catch(err=>{
+    console.log(err)
+  })
+}
+render(){
+  return(
+    <div>
+      {this.state.company}
+    </div>
+  )
+}
+
+
+}
+
 
 export default App;
